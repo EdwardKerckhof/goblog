9b92d165f30c42733a016ce08c3445a9c4f4ecae
EdwardKerckhof <edward@zonedev.be>
2022-04-17 01:14:30 +0200

    Fully test gorm repo

--- api/internal/repositories/post/gorm_test.go
+++ api/internal/repositories/post/gorm_test.go
@@ -47,7 +47,7 @@ func (s *Suite) SetupSuite() {
 		log.New(os.Stdout, "\r\n", log.LstdFlags),
 		logger.Config{
 			SlowThreshold:             time.Second,
-			LogLevel:                  logger.Silent,
+			LogLevel:                  logger.Info,
 			IgnoreRecordNotFoundError: false,
 			Colorful:                  true,
 		},
@@ -90,4 +90,71 @@ func (s *Suite) Test_repository_Get() {
 	res, err := s.repository.Get(post.ID)
 	require.NoError(s.T(), err)
 	require.NotNil(s.T(), res)
+
+	requireEqual(s.T(), post, res)
+}
+
+func (s *Suite) Test_repository_GetAll() {
+	query := `SELECT * FROM "posts"`
+
+	rows := s.mock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "title", "body"}).
+		AddRow(post.ID, post.CreatedAt, post.UpdatedAt, post.DeletedAt, post.Title, post.Body)
+
+	s.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WillReturnRows(rows)
+
+	res, err := s.repository.GetAll()
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), res)
+
+	requireEquals(s.T(), post, res)
+}
+
+func (s *Suite) Test_repository_Create() {
+	query := `
+		INSERT INTO "posts" ("created_at","updated_at","deleted_at","title","body","id")
+		VALUES ($1,$2,$3,$4,$5,$6)
+		RETURNING "id"
+	`
+
+	rows := s.mock.NewRows([]string{"id"}).
+		AddRow(post.ID)
+
+	s.mock.ExpectBegin()
+	s.mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs(post.CreatedAt, post.UpdatedAt, post.DeletedAt, post.Title, post.Body, post.ID).
+		WillReturnRows(rows)
+	s.mock.ExpectCommit()
+
+	res, err := s.repository.Create(&post)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), res)
+
+	requireEqual(s.T(), post, res)
+}
+
+func (s *Suite) Test_repository_Delete() {
+	updateQuery := `UPDATE "posts" SET "deleted_at"=$1 WHERE "posts"."id" = $2`
+
+	s.mock.ExpectBegin()
+	s.mock.ExpectExec(regexp.QuoteMeta(updateQuery)).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	s.mock.ExpectCommit()
+
+	s.repository.Delete(&post)
+}
+
+func requireEqual(t *testing.T, post domain.Post, res *domain.Post) {
+	require.Equal(t, post.ID, res.ID)
+	require.Equal(t, post.Body, res.Body)
+	require.Equal(t, post.Title, res.Title)
+	require.Equal(t, post.CreatedAt, res.CreatedAt)
+	require.Equal(t, post.UpdatedAt, res.UpdatedAt)
+	require.Equal(t, post.DeletedAt, res.DeletedAt)
+}
+
+func requireEquals(t *testing.T, post domain.Post, res []*domain.Post) {
+	for _, result := range res {
+		requireEqual(t, post, result)
+	}
 }
